650b404a544efe0ff50c9b8a1a487cafd290923c
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-06 00:21:15 +0300

    Solution structure changes. Added identity with SQL Server and migrations applied for them.

---
+++ WEB-API.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API.Web.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("GetInfo")]
+        public IActionResult GetInfo()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
--- WEB-API.Web/Startup.cs
+++ WEB-API.Web/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
-namespace WEB_API
+namespace WEB_API.Web
 {
     public class Startup
     {
@@ -32,7 +25,6 @@ namespace WEB_API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -40,9 +32,7 @@ namespace WEB_API
             
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=GetInfo}");
+                endpoints.MapControllers();
             });
             
             app.UseSwagger();
