49c5086ad9d52cdd4e57f90fbe7ee5aaaa7c98c7
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-17 20:59:54 +0300

    IRepo and ProductRepository id DAL project implemented.

---
+++ WEB-API.DAL/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using WEB_API.DAL.Models;
+
+namespace WEB_API.DAL.Repositories
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> GetAll();
+    }
+}
---
+++ WEB-API.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using WEB_API.DAL.Data;
+using WEB_API.DAL.Models;
+
+namespace WEB_API.DAL.Repositories
+{
+    public class ProductRepository: IRepository
+    {
+        private ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
