c0e997804a182daa0d49de70cb61c929ff8bac23
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-30 17:34:38 +0300

    IOrderService.cs interface methods added, all of them now returning Task.

--- WEB-API.Business/Interfaces/IOrderService.cs
+++ WEB-API.Business/Interfaces/IOrderService.cs
@@ -1,13 +1,15 @@
-﻿using WEB_API.Business.BusinessModels;
+﻿using System.Threading.Tasks;
+using WEB_API.Business.BusinessModels;
 using WEB_API.DAL.Models.Enums;
 
 namespace WEB_API.Business.Interfaces
 {
     public interface IOrderService
     {
-        OrderDetailModel AddDetailToOrder(OrderDetailModel orderDetail);
-        OrderDetailModel RemoveDetailFromOrder(OrderDetailModel orderDetail);
-        OrderModel GetOrderById(int id);
-        OrderModel SetOrderStatus(OrderStatuses status);
+        Task<OrderModel> CreateOrder(OrderModel order);
+        Task<OrderModel> AddDetailToOrder(OrderDetailModel orderDetail);
+        Task<OrderModel> RemoveDetailFromOrder(OrderDetailModel orderDetail);
+        Task<OrderModel> GetOrderById(int id);
+        Task<OrderModel> SetOrderStatus(int orderId,OrderStatuses status);
     }
 }
