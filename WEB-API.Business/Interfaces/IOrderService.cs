957c65d64e4cc588184f94dfaecb63ed6301f6ea
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-30 16:39:22 +0300

    IOrderService interface created in business project.

---
+++ WEB-API.Business/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using WEB_API.Business.BusinessModels;
+using WEB_API.DAL.Models.Enums;
+
+namespace WEB_API.Business.Interfaces
+{
+    public interface IOrderService
+    {
+        OrderDetailModel AddDetailToOrder(OrderDetailModel orderDetail);
+        OrderDetailModel RemoveDetailFromOrder(OrderDetailModel orderDetail);
+        OrderModel GetOrderById(int id);
+        OrderModel SetOrderStatus(OrderStatuses status);
+    }
+}
