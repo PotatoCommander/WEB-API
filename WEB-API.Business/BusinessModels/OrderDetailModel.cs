0d322efb6929099b3725edd5a8ed8041322c4a97
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-30 16:23:16 +0300

    OrderDetailModel in Business project implemented.

---
+++ WEB-API.Business/BusinessModels/OrderDetailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_API.Business.BusinessModels
+{
+    public class OrderDetailModel
+    {
+        public int ProductId { get; set; }
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
