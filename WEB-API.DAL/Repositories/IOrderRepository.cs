d4cfd4ee264bff74a92040dd1d55b032d607016f
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-30 15:53:21 +0300

    IOrderRepository implemented.

---
+++ WEB-API.DAL/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using WEB_API.DAL.Models;
+
+namespace WEB_API.DAL.Repositories
+{
+    public interface IOrderRepository
+    {
+        Order CreateOrder(Order order);
+        Order GetOrderById(int id);
+        OrderDetail AddDetailToOrder(OrderDetail detail);
+        OrderDetail DeleteDetailFromOrder(OrderDetail detail);
+    }
+}
