47d0c438396052a0e53e445fcc6e3a7937f0c2fa
Nikita Bodry <bodrik66aa@gmail.com>
2021-06-30 16:23:08 +0300

    OrderModel in Business project implemented.

---
+++ WEB-API.Business/BusinessModels/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using WEB_API.DAL.Models.Enums;
+
+namespace WEB_API.Business.BusinessModels
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public OrderStatuses OrderStatus { get; set; }
+    }
+}
