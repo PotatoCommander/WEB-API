1313535c39692078fc3309d40a09ffc951c728e7
Nikita Bodry <bodrik66aa@gmail.com>
2021-07-01 20:35:44 +0300

    Quantity field in DAL model changed type to uint

---
+++ WEB-API.DAL/Migrations/20210630174800_UIntQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEB_API.DAL.Migrations
+{
+    public partial class UIntQuantity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Quantity",
+                table: "OrderDetails",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "OrderDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
